70434153953bb4837bd8e2a2c27fae6ad38c3bc5
CalebScott0 <calebbenscott@gmail.com>
2024-06-05 19:33:47 -0500

    Block 24 workshop

--- Block24 Workshop/src/App.jsx
+++ Block24 Workshop/src/App.jsx
@@ -1,13 +1,19 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { puppyList } from "./data";
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList);
 
+  console.log("puppies: ", puppies);
   return (
-    <div>
+    <div className="App">
       <h1>Puppy Pals</h1>
+      {puppies.map((puppy) => {
+        return <p key={puppy.id}>{puppy.name}</p>;
+      })}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
